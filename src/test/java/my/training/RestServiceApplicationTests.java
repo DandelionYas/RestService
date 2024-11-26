504a2dae0388b5b791ae406920566276d732ad6d
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2024-11-26 13:19:08 +0330

    Fix test fails for Spring Boot 3.3.6 and Java 21

--- src/test/java/my/training/RestServiceApplicationTests.java
+++ src/test/java/my/training/RestServiceApplicationTests.java
@@ -2,14 +2,12 @@ package my.training;
 
 import my.training.model.Greeting;
 import org.assertj.core.api.BDDAssertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -19,7 +17,6 @@ import static java.lang.String.format;
 import static my.training.controller.GreetingController.TEMPLATE;
 
 //Note that the tests start the application on a random port.
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestServiceApplicationTests {
     private String name = "Yaser";
--- src/test/java/my/training/TestsConfiguration.java
+++ src/test/java/my/training/TestsConfiguration.java
@@ -1,30 +1,16 @@
 package my.training;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
 
 @Configuration
 public class TestsConfiguration {
 
     @Bean
-    public RestTemplate restTemplate() throws Exception {
-        SSLContext sslContext = SSLContextBuilder.create()
-                .setKeyStoreType("PKCS12")
-                .loadTrustMaterial(ResourceUtils.getFile("classpath:ssl-truststore.truststore"),
-                        "changeit".toCharArray())
-                .build();
-
-        HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
-        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        return new RestTemplate(requestFactory);
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder, SslBundles sslBundles) {
+        return restTemplateBuilder.setSslBundle(sslBundles.getBundle("secure-client")).build();
     }
 }
